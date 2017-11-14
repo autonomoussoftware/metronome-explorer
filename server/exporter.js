d943edde8f2233a24fe4b279c8861f1c56ed829a
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-11-14 14:23:03 +0200

    Refactor exporter

--- server/exporter.js
+++ server/exporter.js
@@ -5,119 +5,99 @@ import db from './db'
 import config from './config'
 
 const Exporter = function (config, db) {
-  const self = this
-  self.config = config
-  self.db = db
+  // const self = this
+  this.config = config
+  this.db = db
 
-  self.web3 = new Web3()
-  self.web3.setProvider(config.provider)
+  this.web3 = new Web3()
+  this.web3.setProvider(config.provider)
 
-  console.log('Getting Info for ', config.tokenAddress)
+  console.log(`Getting Info for token: ${config.tokenAddress}`)
 
-  self.contract = self.web3.eth.contract(config.erc20ABI).at(config.tokenAddress)
-  self.allEvents = self.contract.allEvents({fromBlock: config.exportStartBlock, toBlock: 'latest'})
-  self.newEvents = self.contract.allEvents()
+  this.contract = this.web3.eth.contract(config.erc20ABI).at(config.tokenAddress)
+  this.allEvents = this.contract.allEvents({fromBlock: config.exportStartBlock, toBlock: 'latest'})
+  this.newEvents = this.contract.allEvents()
 
-  self.newEvents.watch(function(err, log) { // eslint-disable-line
-    if (err) {
-      console.log('Error receiving new log:', err)
-      return
-    }
+  this.newEvents.watch((err, log) => {
+    if (err) { return console.log(`Error receiving new log: ${err}`) }
     console.log('New log received:', log)
 
-    self.processLog(log, function(err) { // eslint-disable-line
+    this.processLog(log, (err) => {
+      if (err) { return console.log(`Error processing new log: ${err}`) }
       console.log('New log processed')
     })
 
-    if (log.event === 'Transfer') {
-      self.exportBalance(log.args._from)
-      self.exportBalance(log.args._to)
+    if (log.event.toLowerCase() === 'transfer') {
+      this.exportBalance(log.args._from)
+      this.exportBalance(log.args._to)
     }
-    if (log.event === 'Approval') {
-      self.exportBalance(log.args._owner)
-      self.exportBalance(log.args._spender)
+
+    if (log.event.toLowerCase() === 'approval') {
+      this.exportBalance(log.args._owner)
+      this.exportBalance(log.args._spender)
     }
   })
 
-  // Retrieves historical events and processed them
-  self.allEvents.get(function(err, logs) { // eslint-disable-line
-    console.log(logs)
+  this.allEvents.get((err, logs) => {
     console.log('Historical events received')
-    if (err) {
-      console.log('Error receiving historical events:', err)
-      return
-    }
+    if (err) { return console.log('Error receiving historical events:', err) }
+
     const accounts = {}
 
-    logs.forEach(function(log) { // eslint-disable-line
-      if (log.event === 'Transfer') {
+    logs.forEach((log) => {
+      if (log.event.toLowerCase() === 'transfer') {
         accounts[log.args._from] = log.args._from
         accounts[log.args._to] = log.args._to
       }
 
-      if (log.event === 'Approval') {
+      if (log.event.toLowerCase() === 'approval') {
         accounts[log.args._owner] = log.args._owner
         accounts[log.args._spender] = log.args._spender
       }
     })
 
-    async.eachSeries(logs, self.processLog, function(err) { // eslint-disable-line
+    async.eachSeries(logs, this.processLog, (err) => {
       console.log('All historical logs processed')
-      self.exportBatchAccounts(accounts)
+
+      async.eachSeries(
+        accounts,
+        (item, callback) => { this.exportBalance(item, callback) },
+        (err) => { console.log('All historical balances updated') }
+      )
     })
   })
 
-  self.exportBatchAccounts = function(accounts) { // eslint-disable-line
-    async.eachSeries(accounts, function(item, callback) { // eslint-disable-line
-      self.exportBalance(item, callback)
-    }, function(err) { // eslint-disable-line
-      console.log('All historical balances updated')
-    })
-  }
+  this.processLog = (log, cb) => {
+    log._id = `${log.blockNumber}_${log.transactionIndex}_${log.logIndex}`
 
-  self.processLog = function(log, callback) { // eslint-disable-line
-    log._id = log.blockNumber + '_' + log.transactionIndex + '_' + log.logIndex
+    console.log(`Exporting log:', log._id`)
 
-    console.log('Exporting log:', log._id)
-
-    self.web3.eth.getBlock(log.blockNumber, false, function(err, block) { // eslint-disable-line
+    this.db.insert(log, (err, newLogs) => {
       if (err) {
-        console.log('Error retrieving block information for log:', err)
-        callback()
-        return
-      }
-
-      log.timestamp = block.timestamp
-
-      if (log.args && log.args._value) {
-        log.args._value = log.args._value.toNumber()
-      }
-
-      self.db.insert(log, function(err, newLogs) { // eslint-disable-line
-        if (err) {
-          if (err.message.indexOf('unique') !== -1) {
-            console.log(log._id, 'already exported!')
-          } else {
-            console.log('Error inserting log:', err)
-          }
+        if (err.message.indexOf('unique') !== -1) {
+          return console.log(`The log: ${log._id} was already exported!`)
         }
-        callback()
-      })
+
+        console.log(`Error inserting log: ${err}`)
+      }
+
+      cb && cb()
     })
   }
 
-  self.exportBalance = function(address, callback) { // eslint-disable-line
-    console.log('Exporting balance of', address)
-    self.contract.balanceOf(address, function(err, balance) { // eslint-disable-line
+  this.exportBalance = (address, cb) => {
+    console.log(`Exporting balance of ${address}`)
+
+    this.contract.balanceOf(address, (err, balance) => {
       const doc = { _id: address, balance: balance.toNumber() }
-      self.db.update({ _id: doc._id }, doc, { upsert: true }, function(err, numReplaced) { //eslint-disable-line
+
+      this.db.update({ _id: doc._id }, doc, { upsert: true }, (err, numReplaced) => {
         if (err) {
-          console.log('Error updating balance:', err)
-        } else {
-          console.log('Balance export completed')
+          return console.log(`Error updating balance: ${err}`)
         }
 
-        if (callback) { callback() }
+        console.log('Balance export completed')
+        cb && cb()
       })
     })
   }
