60c59c3d75866af1757f1b0124369690b2a7dfaf
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-11-22 10:47:35 -0300

    Remove backpack

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
   head: {
     title: 'starter',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Metronome Explorer' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+
+  loading: { color: '#3B8070' },
 
   css: ['bootstrap/dist/css/bootstrap.css', '~/assets/css/main.css'],
 
