db87bc72f542e84d01921929d57bd1e6f76600df
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-12-28 20:30:56 -0300

    Add favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,10 +4,17 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Metronome Explorer' }
+      { hid: 'description', name: 'description', content: 'Metronome Explorer' },
+      { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/x-icon', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'mask-icon', color: '#5bbad5', href: '/safari-pinned-tab.svg' },
+      { rel: 'manifest', href: '/manifest.json' },
+      { rel: 'manifest', href: '/site.webmanifest' }
     ]
   },
 
