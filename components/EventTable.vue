daacf8d9a3d3b0983cad8fa792f4d6722fd155f0
Ignacio Anaya <ignacio@bloq.com>
2018-01-16 16:36:22 -0300

    Add mobile layout to event table

--- components/EventTable.vue
+++ components/EventTable.vue
@@ -1,36 +1,43 @@
 <template lang="pug">
 div
   table.table.table-responsive
-      thead
+      thead.hidden-md-down
         tr
           th ID
           th Type
+          th Date
           th From
           th To
           th Amount
-          th Date
       tbody
-        tr(v-for='e in events', :key='e._id')
-          td
+        tr(v-for="e in events", :key="e._id")
+          td.inline
+            span.title ID
             nuxt-link(:to="{ name: 'events-id', params: { id: e._id }}") {{ e._id }}
-          td {{ e.metaData.event }}
+          td.inline
+            span.title Type
+            span {{ e.metaData.event }}
+          td.inline
+            span.title Date
+            mtn-text-tooltip(:text="new Date(e.metaData.timestamp * 1000) | moment('from')", :tooltip="new Date(e.metaData.timestamp * 1000) | moment('LLLL')")
           td
+            span.title From
             span(v-if="e.metaData.returnValues && e.metaData.returnValues._from")
               b(v-if="e.metaData.returnValues._from === '0x0000000000000000000000000000000000000000'") MINTER
               nuxt-link(v-else, :to="{ name: 'accounts-address', params: { address: e.metaData.returnValues._from }}") {{ e.metaData.returnValues._from }}
               //- img.clippy(v-clipboard="e.metaData.returnValues._from", src="~/assets/svg/clippy.svg")
             span(v-else) N/A
           td
+            span.title To
             span(v-if="e.metaData.returnValues && e.metaData.returnValues._to")
               b(v-if="e.metaData.returnValues._to === '0x0000000000000000000000000000000000000000'") MINTER
               nuxt-link(:to="{ name: 'accounts-address', params: { address: e.metaData.returnValues._to }}") {{ e.metaData.returnValues._to }}
               //- img.clippy(v-clipboard="e.metaData.returnValues._to", src="~/assets/svg/clippy.svg")
             span(v-else) N/A
           td.text-right
+            span.title Amount
             span(v-if="e.metaData.returnValues") {{ e.metaData.returnValues._value | mtn }}
             span(v-else) N/A
-          td
-            mtn-text-tooltip(:text="new Date(e.metaData.timestamp * 1000) | moment('from')", :tooltip="new Date(e.metaData.timestamp * 1000) | moment('LLLL')")
 
   mtn-pagination(
     v-show="showPagination", :limit="limit", :page-count="events.length"
@@ -75,9 +82,49 @@ export default {
     cursor: copy;
   }
 
-  @media (min-width: 992px) {
+  @media (min-width: 768px) {
     .table-responsive {
       display: table;
+    }
+
+    td {
+      .title {
+        display: none;
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+
+    td {
+      display: block;
+      border-top: none;
+      text-align: left !important;
+      padding: 0;
+      padding-top: 5px;
+
+      .title {
+        margin-right: 5px;
+        color: #e9ecef;
+      }
+
+      &.inline {
+        display: inline-block;
+        padding-top: 15px;
+        margin-right: 15px;
+      }
+
+      &:last-child {
+        padding-bottom: 15px;
+      }
+    }
+
+    tr {
+      border-top: 1px solid #e9ecef;
+
+      &:first-child {
+        border: none;
+      }
     }
   }
 </style>
