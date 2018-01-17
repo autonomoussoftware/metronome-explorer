198fb91cc6bac6f36c5591eea484814dd7fb3f17
Ignacio Anaya <ignacio@bloq.com>
2018-01-17 15:21:05 -0300

    Move main styles to main.scss

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,16 +1,25 @@
 <template lang="pug">
-  section.container
-    img.logo(src="../assets/img/logo.png", alt="Metronome")
+  main
+    mtn-header
+
     h1.title
       | {{ error.statusCode }}
     h2.info
       | {{ error.message }}
     nuxt-link.button(to="/", v-if="error.statusCode === 404")
       | Homepage
+
+    mtn-footer
+
 </template>
 
 <script>
+import MtnFooter from '~/components/Footer.vue'
+import MtnHeader from '~/components/Header.vue'
+
 export default {
+  components: { MtnFooter, MtnHeader },
+
   props: {
     error: { type: Object }
   }
