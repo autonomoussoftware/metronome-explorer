6b383d80eac8ec376651eadc33ea7be338e9380a
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2018-01-22 16:30:25 -0800

    Add status colors

--- components/Status.vue
+++ components/Status.vue
@@ -70,6 +70,10 @@ export default {
 
 
 <style lang="scss" scoped>
+.green { color: green }
+.red { color: tomato }
+.violet { color: #7e61f8 }
+
   @media (max-width: 768px) {
     .container-fluid {
       text-align: left;
