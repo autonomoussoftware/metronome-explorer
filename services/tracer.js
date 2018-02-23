f90b477076049afad3553778d14e58badab0b33a
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2018-02-23 16:10:05 -0300

    Add basic tracer support for transactions

---
+++ services/tracer.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+let options = { baseURL: process.env.tracerUrl }
+
+const tracerApi = axios.create(options)
+const tracer = {}
+
+tracer.transaction = function (transactionHash) {
+  return tracerApi.get(`/transactions/${transactionHash}`)
+    .then(res => res.data)
+}
+
+export default tracer
