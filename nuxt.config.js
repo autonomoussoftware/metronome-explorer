e8c9783cfbbfae483b2beae49c3bc519df0db474
Felix Enriquez <felix@bloq.com>
2018-03-15 11:57:54 -0300

    Fix format

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,101 +1,105 @@
-const path = require("path");
-require("dotenv").config();
+const path = require('path')
+require('dotenv').config()
 
-if (process.env.MEM_WATCH === "true") {
+if (process.env.MEM_WATCH === 'true') {
   const REPORT_PATH = process.env.MEM_DUMP_PATH
     ? process.env.MEM_DUMP_PATH
-    : "./analytics/";
+    : './analytics/'
 
-  var heapdump = require("heapdump");
-  var memwatch = require("memwatch-next");
+  var heapdump = require('heapdump')
+  var memwatch = require('memwatch-next')
 
-  console.log("starting memwatch and heapdump");
+  console.log('starting memwatch and heapdump')
 
   const createSnapshot = dir => {
-    heapdump.writeSnapshot(dir, function(err, filename) {
-      console.log("dump written to", filename);
-    });
-  };
+    heapdump.writeSnapshot(dir, function (err, filename) {
+      if (err) {
+        console.log(err)
+        return
+      }
+      console.log('dump written to', filename)
+    })
+  }
 
-  setInterval(function() {
-    createSnapshot(path.join(REPORT_PATH, Date.now() + ".heapsnapshot"));
-  }, process.env.MEM_WATCH_INTERVAL);
+  setInterval(function () {
+    createSnapshot(path.join(REPORT_PATH, Date.now() + '.heapsnapshot'))
+  }, process.env.MEM_WATCH_INTERVAL)
 
-  memwatch.on("leak", info => {
-    console.error("Memory leak detected:\n", info);
+  memwatch.on('leak', info => {
+    console.error('Memory leak detected:\n', info)
     createSnapshot(
-      path.join(REPORT_PATH, Date.now() + "-leaked" + ".heapsnapshot")
-    );
-  });
+      path.join(REPORT_PATH, Date.now() + '-leaked' + '.heapsnapshot')
+    )
+  })
 }
 
 module.exports = {
   head: {
-    title: "Metronome Explorer",
+    title: 'Metronome Explorer',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: "description",
-        name: "description",
-        content: "Metronome Explorer"
+        hid: 'description',
+        name: 'description',
+        content: 'Metronome Explorer'
       },
-      { name: "theme-color", content: "#ffffff" }
+      { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
-        rel: "icon",
-        type: "image/x-icon",
-        sizes: "32x32",
-        href: "/favicon-32x32.png"
+        rel: 'icon',
+        type: 'image/x-icon',
+        sizes: '32x32',
+        href: '/favicon-32x32.png'
       },
       {
-        rel: "icon",
-        type: "image/x-icon",
-        sizes: "16x16",
-        href: "/favicon-16x16.png"
+        rel: 'icon',
+        type: 'image/x-icon',
+        sizes: '16x16',
+        href: '/favicon-16x16.png'
       },
       {
-        rel: "apple-touch-icon",
-        sizes: "180x180",
-        href: "/apple-touch-icon.png"
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png'
       },
-      { rel: "mask-icon", color: "#5bbad5", href: "/safari-pinned-tab.svg" },
-      { rel: "manifest", href: "/manifest.json" },
-      { rel: "manifest", href: "/site.webmanifest" }
+      { rel: 'mask-icon', color: '#5bbad5', href: '/safari-pinned-tab.svg' },
+      { rel: 'manifest', href: '/manifest.json' },
+      { rel: 'manifest', href: '/site.webmanifest' }
     ]
   },
 
   env: {
-    apiUrl: process.env.MTN_API_URL || "http://localhost:3000",
-    socketUrl: process.env.MTN_SOCKET_URL || "http://localhost:3000",
-    ethUrl: process.env.ETH_NODE_URL || "http://parity.bloqrock.net:8545",
-    tracerUrl: process.env.TRACER_URL || "http://tracer.bloqrock.net"
+    apiUrl: process.env.MTN_API_URL || 'http://localhost:3000',
+    socketUrl: process.env.MTN_SOCKET_URL || 'http://localhost:3000',
+    ethUrl: process.env.ETH_NODE_URL || 'http://parity.bloqrock.net:8545',
+    tracerUrl: process.env.TRACER_URL || 'http://tracer.bloqrock.net'
   },
 
-  serverMiddleware: [{ path: "/health", handler: "~/middlewares/health.js" }],
+  serverMiddleware: [{ path: '/health', handler: '~/middlewares/health.js' }],
 
-  loading: { color: "#7E66F4", height: "3px" },
+  loading: { color: '#7E66F4', height: '3px' },
 
-  css: ["bootstrap/dist/css/bootstrap.css", "~/assets/scss/main.scss"],
+  css: ['bootstrap/dist/css/bootstrap.css', '~/assets/scss/main.scss'],
 
-  modules: ["@nuxtjs/font-awesome"],
+  modules: ['@nuxtjs/font-awesome'],
 
-  plugins: ["~/plugins/filters", "~/plugins/directives"],
+  plugins: ['~/plugins/filters', '~/plugins/directives'],
 
   build: {
-    vendor: ["axios", "socket.io-client"],
+    vendor: ['axios', 'socket.io-client'],
 
-    extend(config, ctx) {
+    extend (config, ctx) {
       if (ctx.isClient) {
         config.module.rules.push({
-          enforce: "pre",
+          enforce: 'pre',
           test: /\.(js|vue)$/,
-          loader: "eslint-loader",
+          loader: 'eslint-loader',
           exclude: /(node_modules)/
-        });
+        })
       }
     }
   }
-};
+}
