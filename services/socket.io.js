1c3620fa7e24de9733e99e3a4bfb06642d3fba4a
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2018-04-17 13:48:57 -0300

    Add basic structure and first test cases for e2e

--- services/socket.io.js
+++ services/socket.io.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
 
-const socket = io(process.env.socketUrl)
+const socket = io(process.env.socketUrl || '')
 
 export default socket
---
+++ test/e2e/events._id.spec.js
@@ -0,0 +1,43 @@
+const test = require('ava')
+const { Nuxt, Builder } = require('nuxt')
+const { resolve } = require('path')
+const nock = require('nock')
+
+const socketServices = require('../../services/socket.io')
+socketServices.on = function () {}
+
+const event = require('../fixtures/event')
+
+let nuxt = null
+
+test.before('Init Nuxt.js', async t => {
+  const rootDir = resolve(__dirname, '../../')
+  let config = {}
+
+  try { config = require(resolve(rootDir, 'nuxt.config.js')) } catch (e) {}
+  config.rootDir = rootDir
+  config.dev = false
+
+  config.css = []
+  config.env.apiUrl = 'http://localhost:3002'
+  config.env.socketUrl = 'http://localhost:3003'
+
+  nuxt = new Nuxt(config)
+  await new Builder(nuxt).build()
+  nuxt.listen(4000, 'localhost')
+})
+
+test('Route / exits and render HTML', async t => {
+  nock('http://localhost:3002')
+    .get(`/event/${event._id}`)
+    .reply(200, event)
+
+  let context = {}
+  const { html } = await nuxt.renderRoute(`/events/${event._id}`, context)
+
+  t.true(html.includes(`Event ${event._id}`))
+})
+
+test.after('Closing server', t => {
+  nuxt.close()
+})
